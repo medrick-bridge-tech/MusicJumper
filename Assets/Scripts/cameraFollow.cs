7f1c0909ff6d29918f921ebd44e6c663221d0d67
Amir Yazdan <begal.cmay@gmail.com>
2023-08-12 11:30:05 +0330

    Camera Follower

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform Target;
+    [SerializeField] float smoothSpeed;
+
+    private Vector3 Offset;
+
+    private void Start()
+    {
+        Offset = transform.position - Target.position;
+    }
+
+    private void Update()
+    {
+        Vector3 Direction = Target.position + Offset;
+
+        Vector3 smoothPositin = Vector3.Lerp(transform.position, Direction, smoothSpeed);
+        transform.position = smoothPositin;
+    }
+}

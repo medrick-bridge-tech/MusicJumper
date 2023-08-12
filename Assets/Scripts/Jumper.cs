f2300955c58036253f541d6f0fb07fb8e0f73b55
Amir Yazdan <begal.cmay@gmail.com>
2023-08-12 11:22:58 +0330

    Player Moves

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class Jumper : MonoBehaviour
 {
-
+    [Header("Player")]
     private Rigidbody2D rigidBody;
-    [SerializeField] float JumpForce;
-    
+    [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeed;
 
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("platform"))
         {
-            Vector2 Velocity = rigidBody.velocity;
-            Velocity.y = JumpForce;
-            rigidBody.velocity = Velocity;
+            Jump();
         }
+    }
+
+    void Jump()
+    {
+        Vector2 Velocity = rigidBody.velocity;
+        Velocity.y = jumpForce;
+        rigidBody.velocity = Velocity;
     }
 
     // Start is called before the first frame update
@@ -29,6 +34,17 @@ public class Jumper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float HorizontalAxis = Input.GetAxisRaw("Horizontal");
+
+        Vector3 Direction = Vector3.zero;
+        Direction.x = HorizontalAxis;
+
+        Vector3 Translate = Direction * moveSpeed * Time.deltaTime;
+        transform.Translate(Translate);
     }
 }

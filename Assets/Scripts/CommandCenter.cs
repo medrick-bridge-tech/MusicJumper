04819e8f9b024c253d1927e07f6fd370b7906f84
Amir Yazdan <begal.cmay@gmail.com>
2023-08-13 20:51:04 +0330

    Add new notes | Create new song (Casatchok)

--- Assets/Scripts/CommandCenter.cs
+++ Assets/Scripts/CommandCenter.cs
@@ -42,7 +42,7 @@ public class CommandCenter : MonoBehaviour
     List<Color> ColorList = new List<Color>();
     List<string> NotesName = new List<string>
     {
-        "A","Bb","B","C1","D1","E1","F1","G1","A1","B1","C2"
+        "A","Bb","B","C1","D1","E1","F1","G1","A1","B1","C2","D2","E2","F2","G2","A2","B2"
     };
 
     private GameObject lastObject;

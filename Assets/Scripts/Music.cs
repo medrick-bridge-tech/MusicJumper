f7b831cd00d8fc6ab81a500e16df4a0eddbd7b9c
Siavash Namjoo <namjoosiavash@outlook.com>
2023-08-21 10:51:30 +0330

    Casatchok scene fixed bugs

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using UnityEditor.Build.Content;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 using UnityEngine.Serialization;

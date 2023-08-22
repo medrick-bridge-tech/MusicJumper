dc62bed8fe8eb561c6f5cbb87e5b6f04d8d5eea2
Siavash Namjoo <namjoosiavash@outlook.com>
2023-08-22 15:36:33 +0330

    Coin Collector and Counter

---
+++ Assets/Scripts/CoinCollector.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollector : MonoBehaviour
+{
+    private int collectedCoin = 0;
+
+    [SerializeField] Text coinCountText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            CollectCoin(other.gameObject);
+        }
+    }
+
+    void CollectCoin(GameObject coin)
+    {
+        collectedCoin++;
+        Destroy(coin);
+        UpdateCoinCountUI();
+    }
+
+    void UpdateCoinCountUI()
+    {
+        if (coinCountText != null)
+        {
+            coinCountText.text = ": " + collectedCoin.ToString();
+        }
+    }
+}
